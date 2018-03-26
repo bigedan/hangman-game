2412f51efc114516d20166b5b362fe2cc366df27
Daniel Nop <danielnop@daniels-air.lan>
2018-03-26 11:20:11 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# hangman-game
+# The Hangman Game
+
+This is a basic hangman game.  The clue for the words are, Georgia cities.
+
+1. Press any key to get started
+
+2. You have 7 guesses, game is over if you exceed the guesses and have not discovered the word.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,40 +2,45 @@ var word = ["atlanta", "doraville", "marietta","mableton","smyrna","kennesaw","g
 
 var wins = 0;
 
-var pWins = function (){
-  wins++;
-}
-//Create a variable for the word.
+$("body").keypress(){
+  $(".contentPanel").removeData();
+};
 
-//Number of guesses remaining for the user
-//Create a variable for guesses
-//Set the variable to 12(optional)
-//If the player misses a letter, derease by 1.
 
-var Guessed = function (pressedKey) {
-  $( "#letters-guessed" ).append( "<span>" + pressedKey + "<span>" );
-}
+// var pWins = function (){
+//   wins++;
+// }
+// //Create a variable for the word.
 
-var pressedLetters = function (event) {
-    var usersGuessedLetter = event.key;
-    //Function checks to see if the letter is in the word
-    //If the letter is in the word, display it on the screen
-    //Else - call the decrease guess Function
-    // call the letter guessed function
+// //Number of guesses remaining for the user
+// //Create a variable for guesses
+// //Set the variable to 12(optional)
+// //If the player misses a letter, derease by 1.
 
-        Guessed(usersGuessedLetter);
-}
+// var guessed = function (pressedKey) {
+//   $( "#letters-guessed" ).append( "<span>" + pressedKey + "<span>" );
+// }
 
-//Display a dash, for each letter in the word
-//Use .length to check the length of the word
-//Create a for loop that puts as many dashes as the selected word.
-$(document).ready(function() {
-  for (var i = 0; i < word.length; i++) {
-      console.log("The loop ran");
-      $( "#answer-box" ).append( "<span>_<span>" );
+// var pressedLetters = function (event) {
+//     var usersGuessedLetter = event.key;
+//     //Function checks to see if the letter is in the word
+//     //If the letter is in the word, display it on the screen
+//     //Else - call the decrease guess Function
+//     // call the letter guessed function
 
-  }
+//         Guessed(usersGuessedLetter);
+// }
 
-})
+// //Display a dash, for each letter in the word
+// //Use .length to check the length of the word
+// //Create a for loop that puts as many dashes as the selected word.
+// $(document).ready(function() {
+//   for (var i = 0; i < word.length; i++) {
+//       console.log("The loop ran");
+//       $( "#answer-box" ).append( "<span>_<span>" );
 
-  
+//   }
+
+// })
+
+//   
