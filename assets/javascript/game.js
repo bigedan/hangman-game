011c3c33101535de0dca91c580400bfa8550c55c
Daniel Nop <danielnop@daniels-air.lan>
2018-03-26 21:48:16 -0400

    startgame and random initialized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,25 @@
-var word = ["atlanta", "doraville", "marietta","mableton","smyrna","kennesaw","gainesville"];
+var gaCities = ["Decatur", "Sherwood Forest", "Johns Creek", "Alparetta", "Buckhead Forest", "Atlanta", "Midtown", "Suwanee", "Roswell", "Peachtree Park"];
 
+// choose words randomly for word array
+var theWord = gaCities[Math.floor(Math.random() * gaCities.length)];
 var wins = 0;
+var guesses = 7;
+var alreadyGuessed = [];
 
-$("body").keypress(){
-  $(".contentPanel").removeData();
+/*==================Functions ========================*/
+
+function startGame(){
+	document.onkeyup = function(e){
+		for (i = 0; i < gaCities.length; i ++){
+    	
+    	$("#theWord").append("Guess ths word: " + theWord);
+    	break;
+    }
+    $("#theWord").append(theWord);
+    $("#wins").append(wins);
+    $("#guesses").append(guesses);
+	}
 };
 
+startGame();
 
-// var pWins = function (){
-//   wins++;
-// }
-// //Create a variable for the word.
-
-// //Number of guesses remaining for the user
-// //Create a variable for guesses
-// //Set the variable to 12(optional)
-// //If the player misses a letter, derease by 1.
-
-// var guessed = function (pressedKey) {
-//   $( "#letters-guessed" ).append( "<span>" + pressedKey + "<span>" );
-// }
-
-// var pressedLetters = function (event) {
-//     var usersGuessedLetter = event.key;
-//     //Function checks to see if the letter is in the word
-//     //If the letter is in the word, display it on the screen
-//     //Else - call the decrease guess Function
-//     // call the letter guessed function
-
-//         Guessed(usersGuessedLetter);
-// }
-
-// //Display a dash, for each letter in the word
-// //Use .length to check the length of the word
-// //Create a for loop that puts as many dashes as the selected word.
-// $(document).ready(function() {
-//   for (var i = 0; i < word.length; i++) {
-//       console.log("The loop ran");
-//       $( "#answer-box" ).append( "<span>_<span>" );
-
-//   }
-
-// })
-
-//   
